c485f406af5902471011800bacc96d442d76d002
MarzukaaZaki <zakimarzuka@gmail.com>
2023-10-04 20:50:08 +0600

    Styled the search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {BsSearch} from 'react-icons/bs'
 
 const SearchBar = ({carData}) => {
 
@@ -22,12 +23,17 @@ const SearchBar = ({carData}) => {
       };
 
     return (
-        <div className='mb-10'>
-            <input 
+        <div className='mb-10 bg-[#f0f4fb] px-6 py-4 shadow-sm'>
+            <div className='flex'>
+                <input 
             type='text' 
             placeholder='Search by car name...'
             value={searchTerm}
-            onChange={handleSearchInputChange}/>
+            onChange={handleSearchInputChange}
+            className='rounded-full px-5 py-2 w-1/3 shadow-sm'/>
+            <BsSearch className='text-[#bdc3c6] font-bold mt-3 -ms-10'/> 
+            </div>
+           
             <ul>
         {searchResults.map((car) => (
           <li key={car.id}>{car.car_name}</li>
